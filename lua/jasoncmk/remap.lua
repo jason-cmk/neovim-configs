e6412713a1289650353708522908bd2bb9bcc73f
Jason Kang <jas.kang@gmail.com>
2023-04-13 19:24:17 +1200

    Add remaps

--- lua/jasoncmk/remap.lua
+++ lua/jasoncmk/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end)
