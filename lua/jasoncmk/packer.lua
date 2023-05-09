f6d8932f97996458b25e5bd3eb9c6f24463cfe9a
Jason Kang <jas.kang@gmail.com>
2023-05-09 17:51:24 +1200

    Add VimBeGood

--- lua/jasoncmk/packer.lua
+++ lua/jasoncmk/packer.lua
@@ -7,6 +7,8 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    use 'ThePrimeagen/vim-be-good'
+
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
