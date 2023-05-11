7c17ef19b08c3f0ee3ee765fcbaaaf681f6ee302
Jason Kang <jas.kang@gmail.com>
2023-05-11 17:14:34 +1200

    Dynamically determine DAP config based on existence of .vscode/launch.json

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -1,43 +1,71 @@
 local dap = require('dap')
 
-local dapPython = require('dap-python')
-dapPython.setup('~\\AppData\\Local\\nvim-data\\mason\\packages\\debugpy\\venv\\Scripts\\pythonw')
+local mason_settings = require "mason.settings"
+local mason_packages_path = mason_settings.current.install_root_dir .. '\\packages'
 
-dap.configurations.lua = {
-    {
-        type = 'nlua',
-        request = 'attach',
-        name = "Attach to running Neovim instance"
-    }
-}
+local setup_dap_configs = function()
+    local dap_python = require('dap-python')
+    dap_python.setup(mason_packages_path .. '\\debugpy\\venv\\Scripts\\pythonw')
 
+    local launch_js_path = vim.fn.getcwd() .. '\\.vscode\\launch.json'
+
+    if vim.loop.fs_stat(launch_js_path) then
+        require('dap.ext.vscode').load_launchjs(nil, { coreclr = { 'cs' } })
+    else
+        dap.configurations.lua = {
+            {
+                type = 'nlua',
+                request = 'attach',
+                name = "Attach to running Neovim instance"
+            }
+        }
+
+        dap.configurations.cs = {
+            {
+                type = "coreclr",
+                name = "launch - netcoredbg",
+                request = "launch",
+                program = function()
+                    local path_prefix = vim.fn.getcwd() .. '\\bin\\Debug\\net7.0\\'
+                    return vim.fn.input('Path to dll: ', path_prefix, 'file')
+                end,
+
+            },
+        }
+    end
+end
+
+vim.keymap.set('n', '<F5>', function()
+    setup_dap_configs()
+    dap.continue()
+end, { desc = 'Start/continue debugging' })
+
+vim.keymap.set('n', '<S-F5>', dap.terminate, { desc = 'Stop debugging' })
+vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Step over' })
+vim.keymap.set('n', '<S-F11>', dap.step_into, { desc = 'Step into' })
+vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Step out' })
+vim.keymap.set('n', '<Leader>b', dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
+vim.keymap.set('n', '<Leader>B', function()
+    local condition = vim.fn.input('Condition: ')
+    dap.set_breakpoint(condition)
+end, { desc = 'Set breakpoint condition' })
+vim.keymap.set('n', '<Leader>dl', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, { desc = 'Set [d]ebug [l]og point' })
+vim.keymap.set('n', '<Leader>dr', dap.repl.toggle, { desc = 'Toggle [d]ebug [r]epl' })
+vim.keymap.set('n', '<Leader>da', dap.run_last, { desc = 'Run [d]ebug [a]gain' })
+
+vim.fn.sign_define('DapBreakpoint', { text = '🚩', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '🐸', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '⛳', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '😵', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapLogPoint', { text = '📝', texthl = '', linehl = '', numhl = '' })
+
+-- Adapters
 dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
 
-vim.keymap.set('n', '<F5>', function() dap.continue() end)
-vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-
 dap.adapters.coreclr = {
     type = 'executable',
-    command = 'C:\\Users\\jaska\\AppData\\Local\\netcoredbg\\netcoredbg',
+    command = mason_packages_path .. '\\netcoredbg\\netcoredbg',
     args = { '--interpreter=vscode' }
-}
-
-dap.configurations.cs = {
-    {
-        type = "coreclr",
-        name = "launch - netcoredbg",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '\\bin\\Debug\\net7.0\\', 'file')
-        end,
-    },
 }
