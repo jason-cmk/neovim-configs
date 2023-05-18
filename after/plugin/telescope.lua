5c80a39ffda0335335bf22ab56e6a6e57e0334d9
Jason Kang <jas.kang@gmail.com>
2023-05-18 17:57:29 +1200

    Telescope remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,12 +3,12 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [f]ind' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find git files' })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[P]roject [s]earch' })
-vim.keymap.set('n', '<leader>bl', builtin.buffers, { desc = '[B]uffer [l]ist' })
+vim.keymap.set('n', '<leader>lb', builtin.buffers, { desc = '[L]ist [B]uffer' })
 vim.keymap.set('n', '<leader>hh', builtin.help_tags, { desc = '[h][h]elp' })
 
-vim.keymap.set('n', '<leader>bs', function()
+vim.keymap.set('n', '<leader>sb', function()
     builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 10,
         previewer = false,
     })
-end, { desc = '[/] Fuzzy [b]uffer [s]earch' })
+end, { desc = '[/] Fuzzy [s]earch [b]uffer' })
