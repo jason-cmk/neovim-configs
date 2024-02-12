6783c430df9ed63250e10a42963865f83e41e62c
Jason Kang <jas.kang@gmail.com>
2024-02-13 11:02:11 +1300

    fix gitignore and add copilot init

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,8 @@
+-- Disable copilot
+vim.b.copilot_enabled = false
+
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
